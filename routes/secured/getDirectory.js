03d028cf4c7073d56de621fe4d6291751dde444c
onurryazici <onurr.yazici@outlook.com>
2020-11-30 03:58:58 -0800

    getUserOwnPermissions added

--- routes/secured/getDirectory.js
+++ routes/secured/getDirectory.js
@@ -1,9 +1,11 @@
 var fs = require('fs');
 var path = require('path');
-const { execSync } = require("child_process");
+const { execSync, exec } = require("child_process");
 exports.getDirectory = function (req,res) {
-    var location = req.body.location;
-    const files = fs.readdirSync(location);
+    var location = encodeURI(req.body.location);
+    var username = req.body.username;
+
+    const files  = fs.readdirSync(location);
     const sorted = files.sort((itemA,itemB)=>{
         var statA = fs.statSync(location + "/" + itemA);
         var statB = fs.statSync(location + "/" + itemB);
@@ -18,19 +20,27 @@ exports.getDirectory = function (req,res) {
             return itemA.localeCompare(itemB); 
         }   
     });
+    
     const outputStack = [];
     sorted.forEach((item) => {
-        var properties = getPropertyOfItem(location + "/" + item);
-        var permissions = getPermissionsOfItem(location + "/" + item)
-        outputStack.push(properties);
+        var itemPath = location + "/" +item;
+
+    
+        var properties  = getPropertyOfItem(itemPath); ////////////////////+++
+        var permissions = getUserOwnPermissionsOfItem(itemPath, username);//++
+        var sharedWith  = getSharedUsersWithPermissionOfItem(itemPath);
+        var output      = Object.assign({},properties, permissions,sharedWith);
+        outputStack.push(output);
     })
     return res.json({items:outputStack});
 }
+
 function getPropertyOfItem(item){
     let properties={
         type:"",
         name:"",
         extension:"",
+        owner:"",
         size:"",
         created:"",
         modified:""
@@ -41,30 +51,104 @@ function getPropertyOfItem(item){
     properties.type      = itemStatu.isDirectory() ? "folder" : "file";
     properties.name      = item.split('/').pop();
     properties.extension = path.extname(item);
+    properties.owner     = getOwnerNameOfItem(item);
     properties.created   = itemStatu.ctime.toLocaleString();
     properties.modified  = itemStatu.mtime.toLocaleString();
     properties.size      = itemSize;
     return properties;
 }
 
-function getPermissionsOfItem(item){
+function getOwnerNameOfItem(item){
+    const itemPath = item.replace(/\s/g,'\\ ');
+    const ownerName = execSync(`ls -ld ${itemPath} | awk {'print $3'}`).toString().trimEnd(); // GET OWNER NAME output:username
+    return ownerName;
+}
+
+function getUserOwnPermissionsOfItem(item,username){
     let permissions = {
-        owner:"",//++
-        read:false,
+        read:false, 
         write:false,
         execute:false,
     }
-    const ownerName = execSync(`ls -ld ${item} | awk {'print $3'}`);
-    //const
+    const itemPath = item.replace(/\s/g,'\\ ');
+    const commandOutput = execSync(`getfacl -cp ${itemPath} | grep 'user::'`).toString().trimEnd();
+    /////// OUTPUT FORMAT //////////////
+    //
+    //     user  :       :   rwx
+    //     [0]      [1]      [2]
+    //
+    ////////////////////////////////////
+    const parsed = commandOutput.split('::');
+    const rwx = parsed[1];
+    
+    (rwx.charAt(0)==="r")?
+        permissions.read = true
+        : permissions.read = false;
+    
+    (rwx.charAt(1)==="w")?
+        permissions.write = true
+        : permissions.read = false;
+    
+    (rwx.charAt(2)==="x" || rwx.charAt(2)==="X")?
+        permissions.execute = true
+        : permissions.read = false;
+    return permissions;
 }
-function getSharedUsersWithPermissionOfItem(){
-    let sharedUsers = [];
+function getSharedUsersWithPermissionOfItem(item){
+    let sharedWith = [];
     let sharedUserStruct = {
         username:"",
         read:false,
         write:false,
         execute:false
     }
+    const itemPath = item.replace(/\s/g,'\\ ');
+    const commandOutput = execSync(`getfacl -cp ${itemPath} | grep 'user'`).toString();
+    // ////////// COMMAND OUTPUT FORMAT /////////////
+    // user     :   user1      :    rwx \n  | line[0]  
+    // user     :   user2      :    r-x \n  | line[1]
+    // user     :   user3      :    --x \n  | line[2]
+    //  data[0] :  data[1]     :    data[2] | .......   
+    /////////////////////////////////////////////////
+
+    console.log("=======================");
+    console.log("=======================");
+    var lines = commandOutput.split('\n');
+    console.log("item path : " + itemPath);
+    console.log(lines);
+    for(var i=0 ; i < lines.length ; i++)
+    {    
+        var data = lines[i].toString().split(':');
+        var username = data[1];
+        var userPermission = data[2];
+        
+        /*userPermission.charAt(0)==="r" ?
+                sharedUserStruct.read = true
+                :
+                sharedUserStruct.read = false;
+            userPermission.charAt(1)==="w" ?
+                sharedUserStruct.write=true
+                :
+                sharedUserStruct.write=false;
+            userPermission.charAt(3)==="x" || userPermission[2]==="X" ?
+                sharedUserStruct.execute=true
+                :
+                sharedUserStruct.execute=false;*/
+        if(username!=='')
+        {
+            sharedUserStruct.username = username;
+            
+        }
+        else{
+            sharedUserStruct.username = getOwnerNameOfItem(item);
+        }
+        sharedWith.push(sharedUserStruct);
+       
+        console.log("------------------------------");
+    }
+    console.log("=======================");
+    console.log("=======================");
+    return sharedWith;
 }
 /*
 [
