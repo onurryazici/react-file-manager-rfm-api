27201bfec3d1271f59c64f87481bee892eb88285
onurryazici <onurr.yazici@outlook.com>
2020-12-12 12:25:47 -0800

    .

--- routes/secured/getDirectory.js
+++ routes/secured/getDirectory.js
@@ -1,8 +1,10 @@
 var fs = require('fs');
 var helpers = require('../../helper/functions/functions');
+
 exports.getDirectory = function (req,res) {
     var encryptedLocation = req.query.location;
-    var username = req.body.username || req.query.username;
+    var username = req.query.username;
+    var showHiddenFiles = (req.query.showHiddenFiles === "yes") ? true : false;
     var location = Buffer.from(encryptedLocation,'base64').toString('ascii');
     const files  = fs.readdirSync(location);
     const sorted = files.sort((itemA,itemB)=>{
@@ -20,14 +22,21 @@ exports.getDirectory = function (req,res) {
         }   
     });
     const outputStack = [];
+    
+    sorted.filter(item => /(^|\/)\.[^/.]/g.test(item));
+    
     sorted.forEach((item) => {
-        var unformattedPath = location + "/" +item;
-        var formattedPath   = unformattedPath.replace(/\s/g,'\\ ');
-        var properties      = helpers.getPropertyOfItem(unformattedPath);
-        var permissions     = helpers.getUserOwnPermissionsOfItem(formattedPath, username);
-        var sharedWith      = helpers.getSharedUsersWithPermissionOfItem(formattedPath);
-        var output          = Object.assign({},properties, permissions,sharedWith)
-        outputStack.push(output);
+        var itemIsHidden  = ( /^\..*/.test(item))
+        if( (showHiddenFiles === false && itemIsHidden === false) || (showHiddenFiles))
+        {
+            var unformattedPath = location + "/" +item;
+            var formattedPath   = unformattedPath.replace(/\s/g,'\\ ');
+            var properties      = helpers.getPropertyOfItem(unformattedPath);
+            var permissions     = helpers.getUserOwnPermissionsOfItem(formattedPath, username);
+            var sharedWith      = helpers.getSharedUsersWithPermissionOfItem(formattedPath);
+            var output          = Object.assign({},properties, permissions,sharedWith)
+            outputStack.push(output);
+        }
     })
     return res.json({items:outputStack});
 }
