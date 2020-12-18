f17ff31d26d4cb79fab44d3c94836723f218bbc2
onurryazici <onurr.yazici@outlook.com>
2020-12-18 13:00:05 -0800

    Auth started

--- helper/functions/functions.js
+++ helper/functions/functions.js
@@ -127,5 +127,25 @@ module.exports = {
             output.sharedUsers.push(struct);
         }
         return output;
+    },
+    isIpBanned: function(userIp){
+        /*
+        **  var command='echo "<password>" | sudo -S "<command that needs a root access>"';
+        **  child_process.execSync(command)
+        */
+
+
+        /// Komut çıktısı yanlış veriyor
+        var command='echo qweqweasd | sudo -S fail2ban-client status sshd | grep "'+ userIp +'"'; /*** ROOT ERİŞİMİ */
+        var commandOutput = spawnSync('sh',['-c',command],{encoding:'utf8'}).stdout;
+        
+        if(commandOutput.length > 0){
+            console.log("AAAAAAAAAAAA" + commandOutput);
+            return true;
+        }
+        else {
+            console.log("BBBBBBBBBBB" + commandOutput);
+            return false;
+        }
     }
 }
--- routes/open-service/user-authentication.js
+++ routes/open-service/user-authentication.js
@@ -1,21 +1,39 @@
 var Client = require('ssh2').Client;
+const { execSync, exec, spawnSync } = require("child_process");
+
+var API_FUNCTIONS = require('../../helper/functions/functions');
 
 exports.userAuthentication = function(req,res){
-    /*var connection = new Client();
-    connection
-        .on('ready',()=>{
-        console.log("started");
+
+    
+    //var ip = req.headers["X-Forwarded-For"] || req.connection.remoteAddress;
+    var ip = "192.168.1.159";
+    var banned = API_FUNCTIONS.isIpBanned(ip);
+    if(banned){
+        return res.json({
+            statu:"banned"
         })
-        .on('error',(err)=>{
-        console.log("error " + err);
-    }).connect({
-        host: '127.0.0.1',
-        port:22,
-        username:'onur',
-        password:'qweqweasd'
-    })*/
-    res.json({
-        test:"test"
-    })
+    }
+    else{
+        var connection = new Client();
+        connection 
+            .on('ready',() => {
+                return res.json({
+                    statu:true,
+                    message:"Giriş başarılı"
+                });
+            })
+            .on('error',(err)=>{
+                console.log("hata var " + err);
+                return res.json({sonuc:"hata var " + err})
+        }).connect({
+            host: '127.0.0.1',
+            port:22,
+            username:'onur',
+            password:'qweqweasd'
+        })
+    }
+    console.log(ip);
+    
     
 }
