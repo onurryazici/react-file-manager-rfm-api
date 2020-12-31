a0ce0b2f87900d0e90d7b34b1ffeb7b81ce5eab6
onurryazici <onurr.yazici@outlook.com>
2020-12-31 13:33:36 -0800

    Rename Item [ADDED]

--- routes/route.js
+++ routes/route.js
@@ -4,10 +4,12 @@ const { getDirectory } = require('./secured/getDirectory');
 const { userAuthentication } = require('./open-service/user-authentication');
 const { createDirectory } = require('./secured/createDirectory');
 const { removeItem } = require('./secured/removeItem');
+const { renameItem } = require('./secured/renameItem');
 
 router.get('/getDirectory',getDirectory ); // POSTA ÇEVİR
 router.post('/userAuthentication', userAuthentication );
 
 router.get('/createDirectory',createDirectory);
 router.get('/removeItem',removeItem);
+router.get('/renameItem',renameItem);
 module.exports = router;
