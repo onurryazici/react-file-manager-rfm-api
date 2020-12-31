48374386ba1dfe307f70cef9e64799654f1c417c
onurryazici <onurr.yazici@outlook.com>
2020-12-31 01:56:41 -0800

    Create Directory [Completed]

--- routes/secured/createDirectory.js
+++ routes/secured/createDirectory.js
@@ -3,29 +3,36 @@ var API = require('../../helper/SSH_SESSION');
 exports.createDirectory = async function (req,res) {
   var SSH_Connection      = API.getSSH();
   var encryptedLocation   = req.query.location;
+  var encrpyedDirName     = req.query.dirname;
   var location            = Buffer.from(encryptedLocation,'base64').toString('ascii');
-  //var formattedCurrentLocation   = location.replace(/\s/g,'\\ ');
-  //var newFolderName              = req.body.newFolderName;
+  var dirname             = Buffer.from(encrpyedDirName,'base64').toString('ascii');
+  
 
+  const newDirectoryPath = location + "/" + dirname;
     if(SSH_Connection !== null && SSH_Connection.isConnected()) 
     {
-      SSH_Connection.connection.sftp((err,sftp)=>{
-            if (err) console.log("hata var");
-            sftp.mkdir("/home/main/err ", function(err) {
-            if (err) {
-              console.log("Failed to create directory!", err);
+      SSH_Connection.connection.sftp((sftp_err,sftp) => {
+            if (sftp_err)
+              res.status(400).json({statu:false,message:"UNKNOWN_ERROR"});
+             
+            sftp.mkdir(newDirectoryPath, function(error) {
+            if (error) {
+              switch(error.code){
+                case 2:
+                  res.status(400).json({statu:false,message:"BAD_DIRECTORY"});
+                  break;
+                case 4:
+                  res.status(400).json({statu:false,message:"DIRECTORY_ALREADY_EXISTS"});
+                  break;
+                default:
+                  res.status(400).json({statu:false,message:"UNKNOWN_ERROR"});
+                  break;
+              }
             } else {
-              console.log("Directory created on SFTP server");
+              res.status(200).json({statu:true,message:"DIRECTORY_CREATE_SUCCESS"});
             }
           });
       });
-        /*var command = `cd ${formattedCurrentLocation} && mkdir ${newFolderName}`;
-        API.executeSshCommand(command).then((data)=>{
-            //res.status(200).json(JSON.parse(data));
-            //console.log(data);  
-        }).catch((err)=>{
-            res.status(400).json({statu:false,message:err})
-        })*/
     }
     else{
         res.json({
