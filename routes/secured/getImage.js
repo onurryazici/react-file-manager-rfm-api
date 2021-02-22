9ca31e9257a7bf22da59a2a720769cb80e1ab369
onurryazici <onurr.yazici@outlook.com>
2021-02-22 13:02:28 -0800

    .

--- routes/secured/getImage.js
+++ routes/secured/getImage.js
@@ -2,36 +2,27 @@ const Messages = require('../../helper/message');
 var API      = require('../../helper/SSH_SESSION');
 var API_FUNCTIONS = require('../../helper/functions');
 exports.getImage = async function (req,res) {
-    // location       : Encrypted folder location with base64
-    // isItRecycleBin : For viewing trash [true,false]
+    // INPUT
+    // absolutePath : Encrypted image path with base64
 
     var SSH_Connection           = API.getSSH();
-    var SSH_User                 = API.getUsername();
-    //var absolutePath             = req.query.absolutePath;
-    //var itemPath                 = Buffer.from(absolutePath,'base64').toString('utf-8');
+    var absolutePath             = Buffer.from(req.query.absolutePath,'base64').toString('utf-8')
     if(SSH_Connection !== null && SSH_Connection.isConnected()) 
     {   
         SSH_Connection.connection.sftp((sftp_err,sftp) => {
             var base64Data = [];
             var dataLength=0;
-            var file = sftp.createReadStream('/home/main/res.jpg')
+            var file = sftp.createReadStream(absolutePath)
             file.on('data',(chunk)=>{
                 base64Data.push(chunk);
                 dataLength += chunk.length;
             })
             file.on('end',()=>{
                 var dataBuffer = Buffer.concat(base64Data,dataLength);
-                const img = Buffer.from(dataBuffer)
-                res.writeHead(200, {
-                    'Content-Type': 'image/png',
-                    'Content-Length': dataLength
-                  });
-                res.end(img)
-                
+                const img = Buffer.from(dataBuffer).toString('base64')
+                res.status(200).json(img)
             })
-
         })
-        
     }
     else{
         res.json({
@@ -40,24 +31,3 @@ exports.getImage = async function (req,res) {
         });
     }
 }
-
-
-/*
-SSH_Connection.connection.sftp((sftp_err,sftp) => {
-            var base64Data = [];
-            var file = sftp.createReadStream('/home/main/res.jpg')
-            file.on('data',(chunk)=>{
-                base64Data.push(chunk);
-            })
-            file.on('end',()=>{
-                const obj = base64Data;
-                const adata  = base64Data[0];
-                console.log(adata.length)
-               res.status(200).json({
-                   statu:true,
-                   fileData:obj[0]
-               })
-            })
-
-        })
-*/
